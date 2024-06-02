c99a6ea0f14413f67bd70b41fa4c927ac0735005
Kaushal Bundel <kaushalbundel@gmail.com>
2024-06-02 22:48:03 +0530

    changes done as per tutorial

--- polls/views.py
+++ polls/views.py
@@ -1,32 +1,58 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
-
-from django.http import HttpResponse
-
-from .models import Question
-
-
-# this is a function based view(FBV), there is also a class based view(CBV)
+from .models import Question, Choice
 
 
-def index(request):
-    """
-    The index page generally shows the most recent content. We will use the same logic here by using the Django's database API(model API))
-    """
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+# Create your views here. These are all function based views.
+#
+# def index(request):
+#     """
+#     The index page generally shows the most recent content. We will use the same logic here by using the Django's database API(model API)
+#     """
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     context = {"latest_question_list": latest_question_list}
+#     return render(request, "polls/index.html", context)
+# # this is a function based view(FBV), there is also a class based view(CBV)
+#
+#
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html", {"question": question})
+#
+#
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {"question": question})
 
+# class based generic views
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
 
-def results(request, question_id):
-    return HttpResponse(f"You are looking at the result of question {question_id}")
+class DetailView(generic.DetailView):
+    template_name = "polls/detail.html"
+    model = Question
 
+class ResultsView(generic.DetailView):
+    template_name = "polls/results.html"
+    model = Question
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html',
+                      {"question": question, "error_message": "Please select correct choice.!"})
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
